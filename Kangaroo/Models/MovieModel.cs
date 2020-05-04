af3d24bb624f1cc75ee19468af17fb5756d2c647
Stephen Odundo <steveremdi@gmail.com>
2020-05-05 00:03:20 +0300

    Added a model class

---
+++ Kangaroo/Models/MovieModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kangaroo.Models
+{
+    public class MovieModel
+    {
+        public Guid Id { get; set; }
+        public string ImdbId { get; set; }
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public float Rating { get; set; }
+        public int Runtime { get; set; }
+        public string[] Genres { get; set; }
+        public string Summary { get; set; }
+        public string TrailerLink { get; set; }
+        public string Language { get; set; }
+        public string Poster { get; set; }
+    }
+}
