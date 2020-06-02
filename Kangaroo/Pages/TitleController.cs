8d38bafd43c8cf20b0463b21547e2846eb761ea9
Stephen Odundo <steveremdi@gmail.com>
2020-06-02 21:13:23 +0300

    Added controller for title page

---
+++ Kangaroo/Pages/TitleController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Kangaroo.Models;
+
+namespace Kangaroo.Pages
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TitleController : ControllerBase
+    {
+        private readonly MovieContext _context;
+
+        public TitleController(MovieContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Title
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MovieModel>>> GetMovies()
+        {
+            return await _context.Movies.ToListAsync();
+        }
+
+        // GET: api/Title/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<MovieModel>> GetMovieModel(int id)
+        {
+            var movieModel = await _context.Movies.FindAsync(id);
+
+            if (movieModel == null)
+            {
+                return NotFound();
+            }
+
+            return movieModel;
+        }
+
+        // PUT: api/Title/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMovieModel(int id, MovieModel movieModel)
+        {
+            if (id != movieModel.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(movieModel).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MovieModelExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Title
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<MovieModel>> PostMovieModel(MovieModel movieModel)
+        {
+            _context.Movies.Add(movieModel);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetMovieModel", new { id = movieModel.Id }, movieModel);
+        }
+
+        // DELETE: api/Title/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<MovieModel>> DeleteMovieModel(int id)
+        {
+            var movieModel = await _context.Movies.FindAsync(id);
+            if (movieModel == null)
+            {
+                return NotFound();
+            }
+
+            _context.Movies.Remove(movieModel);
+            await _context.SaveChangesAsync();
+
+            return movieModel;
+        }
+
+        private bool MovieModelExists(int id)
+        {
+            return _context.Movies.Any(e => e.Id == id);
+        }
+    }
+}
